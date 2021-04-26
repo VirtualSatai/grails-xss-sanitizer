7cc0006be723e31de5a7976939061d2e560879dd
Troels Beck Krøgh <tbk@netcompany.com>
2021-04-26 13:59:10 +0200

    Dummy commit

--- src/main/java/grails/plugin/xss/sanitizer/util/XssSanitizerUtil.java
+++ src/main/java/grails/plugin/xss/sanitizer/util/XssSanitizerUtil.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+
 /**
  * Taken from http://ricardozuasti.com/2012/stronger-anti-cross-site-scripting-xss-filter-for-java-web-apps/
  */
